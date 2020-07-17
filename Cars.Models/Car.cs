bed3070bfe969c49dbc8eac1303aaf5cb3ce9b88
Roko <rokokovac98@gmail.com>
2020-07-17 13:41:51 +0200

    Add Car and Manufacturer models

---
+++ Cars.Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Cars.Models
+{
+    class Car
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Model { get; set; }
+
+        public Manufacturer Manufacturer { get; set; }
+    }
+}
---
+++ Cars.Models/Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cars.Models
+{
+    class Manufacturer
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
