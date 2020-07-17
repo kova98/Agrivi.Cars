c46b1daf8c269c494675828616be5a1ed4de2ac5
Roko <rokokovac98@gmail.com>
2020-07-17 13:49:09 +0200

    Add Car and Manufacturer DTO-s

---
+++ Cars.DTO/CarDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars.DTO
+{
+    class CarDto
+    {
+        public long Id { get; set; }
+        public string Model { get; set; }
+        public long ManufacturerId { get; set; }
+    }
+}
